65a444350228c61132032d938611a8ddc2b5c3ed
Sara Lin <saralin@dropbox.com>
2015-06-13 20:22:48 -0700

    Added basic local notification sound

--- dreamcatcher/HomeViewController.swift
+++ dreamcatcher/HomeViewController.swift
@@ -1,35 +1,38 @@
 //
-//  AlarmViewController.swift
+//  HomeViewController.swift
 //  dreamcatcher
 //
-//  Created by Sara Lin on 6/11/15.
+//  Created by Sara Lin on 6/13/15.
 //  Copyright (c) 2015 Francisco Guzman. All rights reserved.
 //
 
 import UIKit
 
-class AlarmViewController: UIViewController {
+class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        var date = NSDate()
+        date = date.dateByAddingTimeInterval(10)
+        setLocalNotification(date)
+    }
+    
+    func setLocalNotification (firedate: NSDate) {
+        var notification: UILocalNotification = UILocalNotification()
+        notification.fireDate = firedate
+        notification.alertBody = "It's a new day!"
+        notification.soundName = "alarm_sound_3.mp3"
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-    @IBAction func onPressCloseButton(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    @IBAction func onPressSetButton(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    
     /*
     // MARK: - Navigation
 
