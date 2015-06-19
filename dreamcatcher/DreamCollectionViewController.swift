823715c0ca43e173edc1dbcddeab41239c8be51c
elissah1217 <elissah1217@hotmail.com>
2015-06-19 10:05:28 -0700

    paging with transition

--- dreamcatcher/DreamCollectionViewController.swift
+++ dreamcatcher/DreamCollectionViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class DreamCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class DreamCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIPageViewControllerDataSource {
     
     var journalTransition : JournalTransition!
     var currentRowIndex: NSIndexPath!
     var fadeTransition: FadeTransition!
 
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var pageViewController: UIPageViewController!
     
     // Nav buttons
     @IBOutlet weak var settingButton: UIButton!
@@ -24,10 +26,10 @@ class DreamCollectionViewController: UIViewController, UICollectionViewDataSourc
     
     var numOfCell = 2
     var dateArray = ["JUNE 17", "JUNE 14"]
-    var titleArray = ["Underwater world with corals", "Fat Cat Getting Stuck in the Wall"]
+    var titleArray = ["Underwater world with corals", "Diamond Man"]
     var paragraphArray = [
         "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, ",
-        "My fat cat is so obese that it resembles an overstuff sausage. There were a few times..."
+        "We were lined up at the gate. The guards took everything from us, and checked us before pushing us into the celler. When it was my turn, they checked all my pockets, making sure I had no valuables on me. Then they pasted a piece of tiny diamond on my thumb. \"If you ever find the Diamond Man. Bring him to us. Your freedom will be granted.\" "
     ]
     
     let defaultNavAlpha: CGFloat = 0.3
@@ -46,6 +48,9 @@ class DreamCollectionViewController: UIViewController, UICollectionViewDataSourc
         alarmButton.alpha = defaultNavAlpha
         composeButton.alpha = defaultNavAlpha
         alarmTimeLabel.alpha = 0
+        
+        pageViewController = UIPageViewController(transitionStyle: UIPageViewControllerTransitionStyle.Scroll, navigationOrientation: UIPageViewControllerNavigationOrientation.Horizontal, options: nil)
+        pageViewController.dataSource = self
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -90,7 +95,18 @@ class DreamCollectionViewController: UIViewController, UICollectionViewDataSourc
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath){
         currentRowIndex = indexPath
         NSLog("You selected cell number: \(currentRowIndex.row)!")
-        performSegueWithIdentifier("journalDetailSegue", sender: nil)
+
+        // This acts like pageViewController.reloadData
+        pageViewController.dataSource = nil;
+        pageViewController.dataSource = self;
+        
+        pageViewController.setViewControllers([journalViewControllerAtIndex(currentRowIndex.row)], direction: UIPageViewControllerNavigationDirection.Forward, animated: true, completion: nil)
+
+        journalTransition = JournalTransition()
+        pageViewController.modalPresentationStyle = UIModalPresentationStyle.Custom
+        pageViewController.transitioningDelegate = journalTransition
+        presentViewController(pageViewController, animated: true, completion: nil)
+    
     }
     
     
@@ -105,8 +121,35 @@ class DreamCollectionViewController: UIViewController, UICollectionViewDataSourc
         return cell
     }
     
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        var journalViewController = viewController as! JournalViewController
+        if journalViewController.index > 1 {
+            return journalViewControllerAtIndex(journalViewController.index - 1)
+        }
+        
+        return nil
+    }
     
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        var journalViewController = viewController as! JournalViewController
+        if journalViewController.index < dateArray.count - 1 {
+            return journalViewControllerAtIndex(journalViewController.index + 1)
+        }
+
+        return nil
+    }
     
+    func journalViewControllerAtIndex(index: Int) -> JournalViewController {
+        var storyboard = UIStoryboard(name: "Main", bundle: nil)
+        var journalViewController = storyboard.instantiateViewControllerWithIdentifier("JournalViewController") as! JournalViewController
+
+        journalViewController.paragraph = paragraphArray[index]
+        journalViewController.titleText = titleArray[index]
+        journalViewController.dateText = dateArray[index]
+        journalViewController.index = index
+        
+        return journalViewController
+    }
    
     
     
@@ -134,12 +177,20 @@ class DreamCollectionViewController: UIViewController, UICollectionViewDataSourc
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         
-        if segue.identifier == "journalDetailSegue" {
-            var destinationVC = segue.destinationViewController as! JournalViewController
-            destinationVC.modalPresentationStyle = UIModalPresentationStyle.Custom
-            journalTransition = JournalTransition()
-            destinationVC.transitioningDelegate = journalTransition
-        }
+//        if segue.identifier == "journalDetailSegue" {
+//            var destinationVC = segue.destinationViewController as! JournalViewController
+//            destinationVC.modalPresentationStyle = UIModalPresentationStyle.Custom
+//            journalTransition = JournalTransition()
+//            destinationVC.transitioningDelegate = journalTransition
+//            var currentCell = collectionView.cellForItemAtIndexPath(currentRowIndex) as! CardCollectionViewCell
+//            
+//            println(destinationVC.textView)
+//            
+//            destinationVC.paragraph = currentCell.textView.text
+//            destinationVC.titleText = currentCell.titleLabel.text
+//            destinationVC.dateText = currentCell.dateLabel.text
+//            
+//        }
         
         if segue.identifier == "alarmSegue" {
             var alarmVC = segue.destinationViewController as! AlarmViewController
